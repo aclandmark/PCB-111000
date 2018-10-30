a1cd7a451da066897f384557d1ea784a1c030d2f
aclandmark <mark.osborne15@btinternet.com>
2018-10-30 19:49:45 +0000

    Update 3_mini-OS/I2C_V12_0_CC/.dep/I2C_V12_0_CC.o.d

---
+++ 3_mini-OS/I2C_V12_0_CC/.dep/I2C_V12_0_CC.o.d
@@ -0,0 +1,66 @@
+I2C_V12_0_CC.o: I2C_V12_0_CC.c I2C_V12_0_Header_file_CC.h \
+  Resources_CC/I2C_V12_0_CC.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom328p.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/wdt.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/eeprom.h \
+  Resources_CC/I2C_V12_0_CC_IO_subroutines.c \
+  Resources_CC/I2C_V12_0_CC_display_subroutines.c \
+  Resources_CC/I2C_V12_0_CC_Arithmetic_subroutines.c \
+  Resources_CC/I2C_V12_0_CC_ISR_subroutines.c \
+  Resources_CC/I2C_V12_0_CC_osccal_calibration\ subroutintes.c
+
+I2C_V12_0_Header_file_CC.h:
+
+Resources_CC/I2C_V12_0_CC.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom328p.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/wdt.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/eeprom.h:
+
+Resources_CC/I2C_V12_0_CC_IO_subroutines.c:
+
+Resources_CC/I2C_V12_0_CC_display_subroutines.c:
+
+Resources_CC/I2C_V12_0_CC_Arithmetic_subroutines.c:
+
+Resources_CC/I2C_V12_0_CC_ISR_subroutines.c:
+
+Resources_CC/I2C_V12_0_CC_osccal_calibration\ subroutintes.c:
