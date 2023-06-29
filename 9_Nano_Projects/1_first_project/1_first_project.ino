d1e99650b2c4413f80a1119ad8e5d4259202d16f
aclandmark <mark.osborne15@btinternet.com>
2023-06-29 16:13:59 +0100

    Update 1_first_project.ino

--- 9_Nano_Projects/1_first_project/1_first_project.ino
+++ 9_Nano_Projects/1_first_project/1_first_project.ino
@@ -1,4 +1,6 @@
 
+#define switch_1_down  ((PIND & 0x80)^0x80)
+
 #include "First_project_header.h"
 
 
@@ -14,5 +16,11 @@ while(1){                   //While loop starts here
     I2C_Tx_2_integers(PORT_1, PORT_1);
     Timer_T0_10mS_delay_x_m(6);
     PORT_1 = PORT_1 << 1;}          //for loop ends here
+    
+    if (switch_1_down)
+{Num_to_PC(16, eeprom_read_byte((uint8_t*) 0x1FF));String_to_PC("\r\n"); 
+Num_to_PC(16, eeprom_read_byte((uint8_t*) 0x1FE)); String_to_PC("\r\n");
+ Num_to_PC(16, eeprom_read_byte((uint8_t*) 0x1FD)); String_to_PC("\r\n");}  
+    
     }                   //while loop ends here
 }                       //end of main loop
