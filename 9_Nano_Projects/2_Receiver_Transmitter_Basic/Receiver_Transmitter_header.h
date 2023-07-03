df9a1de4994043378b5587a4a8675489827dd3b4
aclandmark <mark.osborne15@btinternet.com>
2023-07-03 22:25:39 +0100

    Create Receiver_Transmitter_header.h

---
+++ 9_Nano_Projects/2_Receiver_Transmitter_Basic/Receiver_Transmitter_header.h
@@ -0,0 +1,12 @@
+
+
+#include "Resources_nano_projects/Header_files/Basic_HW_setup.h"
+#include "Resources_nano_projects/Subroutines/Basic_HW_plus_Timer.c"
+#include "Resources_nano_projects/Subroutines/Basic_PC_comms.c"
+#include "Resources_nano_projects/I2C_Subroutines/I2C_subroutines_1.c"
+#include "Resources_nano_projects/I2C_Subroutines/I2C_slave_Rx_Tx.c"
+#include "Resources_nano_projects/I2C_Subroutines/I2C_diagnostic.c"
+
+
+#include "Resources_nano_projects/Subroutines/Data_to_from_EEPROMs.c"
+#include "Resources_nano_projects/Subroutines/Random_and_prime_nos.c"
