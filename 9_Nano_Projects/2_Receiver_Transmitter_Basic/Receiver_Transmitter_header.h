9ae006a4115e5a84675d9fbc45a4da5639e977c2
aclandmark <mark.osborne15@btinternet.com>
2023-07-04 20:47:05 +0100

    Update Receiver_Transmitter_header.h

--- 9_Nano_Projects/2_Receiver_Transmitter_Basic/Receiver_Transmitter_header.h
+++ 9_Nano_Projects/2_Receiver_Transmitter_Basic/Receiver_Transmitter_header.h
@@ -1,12 +1,18 @@
 
 
 #include "Resources_nano_projects/Header_files/Basic_HW_setup.h"
-#include "Resources_nano_projects/Subroutines/Basic_HW_plus_Timer.c"
-#include "Resources_nano_projects/Subroutines/Basic_PC_comms.c"
+#include "Resources_nano_projects/Subroutines/HW_timers.c"
+//#include "Resources_nano_projects/Subroutines/Basic_PC_comms.c"
+
+
+#include "Resources_nano_projects/PC_comms/Basic_Rx_Tx_Basic.c"
+
+
 #include "Resources_nano_projects/I2C_Subroutines/I2C_subroutines_1.c"
+#include "Resources_nano_projects/I2C_Subroutines/I2C_subroutines_2.c"
 #include "Resources_nano_projects/I2C_Subroutines/I2C_slave_Rx_Tx.c"
 #include "Resources_nano_projects/I2C_Subroutines/I2C_diagnostic.c"
 
 
-#include "Resources_nano_projects/Subroutines/Data_to_from_EEPROMs.c"
+//#include "Resources_nano_projects/Subroutines/Data_to_from_EEPROMs.c"
 #include "Resources_nano_projects/Subroutines/Random_and_prime_nos.c"
